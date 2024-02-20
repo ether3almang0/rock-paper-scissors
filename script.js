8732921c4c276403fd3cd6a63b0d2412c9cd7e5a
ether3almang0 <zainab.s.live@gmail.com>
2024-02-20 08:13:52 -0500

    final project without cases sensitivity

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function getComputerChoice () {
    let choice = choices[Math.floor(Math.random()*choices.length)];
    return choice;
 }
-console.log(getComputerChoice());
-/*function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
+        return playerSelection + " beats " + computerSelection + "\nYay! You win!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "rock" && computerSelection == "paper") {
+        return computerSelection + " beats " + playerSelection + "\nBoo! You loose!";
+    }
+    else {
+        return "Tis a draw!";
+    }
 }
-    const playerSelection = "rock";
+    let playerSelection = prompt();
     const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection)); */
+    console.log(playRound(playerSelection, computerSelection));
